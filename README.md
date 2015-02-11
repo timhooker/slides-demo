556f1640ae16ad2252505cb958f2c4398db88179
Tim Hooker <timhooker@me.com>
2015-02-11 08:29:58 -0500

    Update 2015-02-11T13:29:57.085Z

--- README.md
+++
@@ -1,3 +0,0 @@
-# Week 5
-
-This is where I'll be putting all of my code for week 5!!
--- src/js/init.js
+++
@@ -1,6 +0,0 @@
-$(function () {
-  var page = $('body').data('page');
-  app[page]();
-  app.resizeItems();
-  app.slideReorder();
-});
